d8e9ebd5cf2ebd6165156d8c517a7d1756a8b291
devNikolasPavl <112695803+devNikolasPavl@users.noreply.github.com>
2024-05-27 17:57:53 +0300

    Skill, написание первого класса с методом

--- src/ru/book_01/project_01/lesson_01/Example.java
+++ src/ru/book_01/project_01/lesson_01/Example.java
@@ -2,6 +2,7 @@ package ru.book_01.project_01.lesson_01;
 
 public class Example {
     public static void main(String[] args) {
+
         System.out.println("Простая программа на языке Java");
     }
 }
---
+++ src/ru/sait_01/project_02/lesson_01/Cookie.java
@@ -0,0 +1,10 @@
+package ru.sait_01.project_02.lesson_01;
+
+public class Cookie {
+    // Метод для выпекания
+
+    public void bake() {
+        // Выводим текст в консоль
+        System.out.println("Печенька");
+    }
+}
