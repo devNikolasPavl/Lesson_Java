b3f8d8e911aa9ba3d56fb6254c033e9e90dd1ba8
devNikolasPavl <112695803+devNikolasPavl@users.noreply.github.com>
2024-06-26 17:45:25 +0300

    video_01, первая программа

---
+++ src/ru/video_01/project_01/lesson_01/Prog1.java
@@ -0,0 +1,14 @@
+package ru.video_01.project_01.lesson_01;
+
+import javax.swing.*;
+
+public class Prog1 {
+    public static void main(String[] args) {
+
+        JFrame nf = new JFrame();
+        nf.setBounds(500, 100, 500, 300);
+        nf.setTitle("Добро пожаловать на изучение Java");
+        nf.setVisible(true);
+
+    }
+}
