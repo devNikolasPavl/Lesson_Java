8c4355a281bca7468d0f73b1f1a3622363133143
devNikolasPavl <112695803+devNikolasPavl@users.noreply.github.com>
2024-08-12 15:50:23 +0300

    level_02, lesson_01

---
+++ ru/javarush/syntax/level_02/lesson_01/Solution.java
@@ -0,0 +1,10 @@
+package ru.javarush.syntax.level_02.lesson_01;
+
+public class Solution {
+    public static void main(String[] args) {
+        int centimeters = 243;
+
+        int meters = centimeters / 100;
+        System.out.println(meters);
+    }
+}
