f216b9817bccce45112fae30f4fc8b87fa55afbd
devNikolasPavl <112695803+devNikolasPavl@users.noreply.github.com>
2024-08-12 15:52:02 +0300

    level_02, lesson_02

---
+++ ru/javarush/syntax/level_02/lesson_02/Solution.java
@@ -0,0 +1,10 @@
+package ru.javarush.syntax.level_02.lesson_02;
+
+public class Solution {
+    public static void main(String[] args) {
+        int number = 243;
+
+        int lastDigit = number % 10;
+        System.out.println(lastDigit);
+    }
+}
