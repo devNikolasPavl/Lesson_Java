7508bf41c903a54c452f6e6b839e4963e9b35b40
devNikolasPavl <112695803+devNikolasPavl@users.noreply.github.com>
2024-08-12 08:26:31 +0300

    Первая программа на Java

---
+++ ru/javarush/syntax/level_01/lesson_01/Hello.java
@@ -0,0 +1,8 @@
+package ru.javarush.syntax.level_01.lesson_01;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Привет, мир!");
+    }
+}
