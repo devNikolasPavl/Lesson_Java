c761b29714865fb9850c884a9b3550ddc04cd86e
devNikolasPavl <112695803+devNikolasPavl@users.noreply.github.com>
2024-08-12 08:59:01 +0300

    level_01, lesson_02

---
+++ ru/javarush/syntax/level_01/lesson_02/Home.java
@@ -0,0 +1,14 @@
+package ru.javarush.syntax.level_01.lesson_02;
+
+// Два вида комментариев - это первый
+
+public class Home {
+    public static void main(String[] args) {
+        /*
+        Второй вид - тут мы выводим фразу 'Amigo the Best'
+         */
+        System.out.print("Amigo ");
+        System.out.print("the ");
+        System.out.print("Best");
+    }
+}
