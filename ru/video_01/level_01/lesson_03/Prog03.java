c50d520d2fcb56a3f29dbfcc8a785f3458e06ce6
devNikolasPavl <112695803+devNikolasPavl@users.noreply.github.com>
2024-08-21 15:54:09 +0300

    Video_01, Level_02 lesson_01

---
+++ ru/video_01/level_01/lesson_03/Prog03.java
@@ -0,0 +1,20 @@
+package ru.video_01.level_01.lesson_03;
+
+import javax.swing.*;
+
+public class Prog03 {
+    public static void main(String[] args) {
+
+        // переменные
+        int x1 = 10;
+        x1 = 20;
+        int x2  = 10;
+
+
+        System.out.println("Добро пожаловать!");
+        // передача сообщений пользователю
+        JOptionPane.showMessageDialog(null, x1 + x2);
+
+    }
+
+}
