f48950f263ceb7f77d3814fb70615e530739bb75
devNikolasPavl <112695803+devNikolasPavl@users.noreply.github.com>
2024-08-26 11:43:25 +0300

    Skill_01, Level_02, lesson_07

---
+++ ru/skillfact/chp_01/level_02/lesson_07/Main.java
@@ -0,0 +1,10 @@
+package ru.skillfact.chp_01.level_02.lesson_07;
+
+public class Main {
+    public static void main(String[] args) {
+        int i = 100;
+        do {
+            System.out.println("Я выполнился");
+        } while (i < 10);
+    }
+}
