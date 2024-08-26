2790e22cc6a3957a8b06eb58d2d5a67b800be055
devNikolasPavl <112695803+devNikolasPavl@users.noreply.github.com>
2024-08-26 11:19:24 +0300

    Skill_01, Level_02, lesson_06

---
+++ ru/skillfact/chp_01/level_02/lesson_06/Main.java
@@ -0,0 +1,11 @@
+package ru.skillfact.chp_01.level_02.lesson_06;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int a = 0;
+        while (a <= 10) {
+            System.out.println(a++);
+        }
+    }
+}
