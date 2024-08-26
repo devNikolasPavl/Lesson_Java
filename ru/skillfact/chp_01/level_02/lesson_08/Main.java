1ad607de1fdf36164020a9c41474a3227769ef17
devNikolasPavl <112695803+devNikolasPavl@users.noreply.github.com>
2024-08-26 11:45:41 +0300

    Skill_01, Level_02, lesson_08

---
+++ ru/skillfact/chp_01/level_02/lesson_08/Main.java
@@ -0,0 +1,9 @@
+package ru.skillfact.chp_01.level_02.lesson_08;
+
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i += 10) {
+            System.out.println("Строка " + i);
+        }
+    }
+}
