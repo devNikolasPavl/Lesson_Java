93cde9048549db7ebd1e3f69587b67bd620e6800
Nikolas <devnikolaspavl@gmail.com>
2024-08-29 16:22:30 +0300

    JavaBegin, level_01, lesson_02

---
+++ ru/javabegin/level_01/lesson_02/Car.java
@@ -0,0 +1,16 @@
+package ru.javabegin.level_01.lesson_02;
+
+public class Car {
+
+    String name;
+    String model;
+    String color;
+
+    void drive() {
+        System.out.println("drive");
+    }
+
+    void stop() {
+        System.out.println("Stop");
+    }
+}
