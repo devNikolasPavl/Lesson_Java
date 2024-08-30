1750f7f32b5f2c0e750921e683028e8b74d3e689
Nikolas <devnikolaspavl@gmail.com>
2024-08-30 10:03:28 +0300

    JavaBegin, level_01, lesson_03

---
+++ ru/javabegin/level_01/lesson_03/Computer.java
@@ -0,0 +1,13 @@
+package ru.javabegin.level_01.lesson_03;
+
+public class Computer {
+    String name = "IBM";
+
+    void on() {
+        System.out.println(name + " computer is on");
+    }
+
+    void off() {
+        System.out.println(name + " computer is off");
+    }
+}
---
+++ ru/javabegin/level_01/lesson_03/Main.java
@@ -0,0 +1,12 @@
+package ru.javabegin.level_01.lesson_03;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Computer computer = new Computer();
+
+        computer.on();
+        computer.off();
+
+    }
+}
