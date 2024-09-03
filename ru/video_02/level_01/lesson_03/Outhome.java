dd39cdf4ca1ab0067f252515f1f9b2078b3a3e68
Nikolas <devnikolaspavl@gmail.com>
2024-09-03 16:44:37 +0300

    video_02, level_01, lesson_03

---
+++ ru/video_02/level_01/lesson_03/Outhome.java
@@ -0,0 +1,14 @@
+package ru.video_02.level_01.lesson_03;
+
+public class Outhome {
+    public static void main(String[] args) {
+        System.out.print(0);
+        System.out.print(3);
+        System.out.print(6);
+        System.out.print(9);
+        System.out.println(0);
+        System.out.println(3);
+        System.out.println(6);
+        System.out.println(9);
+    }
+}
