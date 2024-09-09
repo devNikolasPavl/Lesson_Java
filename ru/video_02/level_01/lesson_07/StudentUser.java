f5ddb49a120bb631e9a86f990566234a86b2d6ac
Nikolas Pavlov <devnikolaspavl@gmail.com>
2024-09-09 11:22:31 +0300

    video_02, level_01, lesson_06

---
+++ ru/video_02/level_01/lesson_07/StudentUser.java
@@ -0,0 +1,10 @@
+package ru.video_02.level_01.lesson_07;
+
+public class StudentUser {
+    public static void main(String[] args) {
+
+        var name = "Nik";
+
+        System.out.println("Вас зовут " + name);
+    }
+}
