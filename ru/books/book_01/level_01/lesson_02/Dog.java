e56fd7006ada0ee6391ffa049343aabb389a6129
devNikolasPavl <112695803+devNikolasPavl@users.noreply.github.com>
2024-09-17 15:45:58 +0300

    book_01, level_01, lesson_02

---
+++ ru/books/book_01/level_01/lesson_02/Dog.java
@@ -0,0 +1,11 @@
+package ru.books.book_01.level_01.lesson_02;
+
+public class Dog {
+    int size;
+    String breed;
+    String name;
+
+    void bark() {
+        System.out.println("Гав! Гав!");
+    }
+}
---
+++ ru/books/book_01/level_01/lesson_02/DogTestDrive.java
@@ -0,0 +1,13 @@
+package ru.books.book_01.level_01.lesson_02;
+
+public class DogTestDrive {
+    public static void main(String[] args) {
+        // Проверочный код для класса Dog
+        // Создаем объект класса Dog
+        Dog d = new Dog();
+
+        // Используем оператор доступа. Устанавлеваем значение поля size
+        d.size = 40;
+        d.bark();
+    }
+}
