86dbfdee03fae7de9846d5b92355d083752f6bc2
devNikolasPavl <112695803+devNikolasPavl@users.noreply.github.com>
2024-09-17 17:23:15 +0300

    book_02, level_02, lesson_01

---
+++ ru/books/book_02/level_02/lesson_01/ShowMeWindowDemo.java
@@ -0,0 +1,10 @@
+package ru.books.book_02.level_02.lesson_01;
+
+import javax.swing.*;
+
+public class ShowMeWindowDemo {
+    public static void main(String[] args) {
+
+        JOptionPane.showMessageDialog(null, "Первая программа на Java");
+    }
+}
