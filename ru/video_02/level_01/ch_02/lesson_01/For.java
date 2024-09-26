f9131be3d0bdaece3c8601f18a4216819cd98af6
devNikolasPavl <112695803+devNikolasPavl@users.noreply.github.com>
2024-09-26 14:05:31 +0300

    video_02, ch_02, lesson_01

---
+++ ru/video_02/level_01/ch_02/lesson_01/For.java
@@ -0,0 +1,16 @@
+package ru.video_02.level_01.ch_02.lesson_01;
+
+public class For {
+    public static void main(String[] args) {
+
+//        System.out.println("Привет");
+//        System.out.println("Привет");
+//        System.out.println("Привет");
+//        System.out.println("Привет");
+//        System.out.println("Привет");
+
+        for (int x = 1; x <= 5; x++) {
+            System.out.println("Привет");
+        }
+    }
+}
