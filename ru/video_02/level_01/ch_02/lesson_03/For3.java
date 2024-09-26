1a1c46d0e09a3fb7c18f8d18dc0f91b27dcd0599
devNikolasPavl <112695803+devNikolasPavl@users.noreply.github.com>
2024-09-26 14:22:20 +0300

    video_02, ch_02, lesson_03

---
+++ ru/video_02/level_01/ch_02/lesson_03/For3.java
@@ -0,0 +1,11 @@
+package ru.video_02.level_01.ch_02.lesson_03;
+
+public class For3 {
+    public static void main(String[] args) {
+
+        // с шагом 5
+        for (int i = 0; i <= 100; i+=5) {
+            System.out.print(i + " ");
+        }
+    }
+}
