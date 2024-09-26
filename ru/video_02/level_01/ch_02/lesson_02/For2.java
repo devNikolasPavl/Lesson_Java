dc6ab0ca285aa96de61051522c7c69eeb2ac5394
devNikolasPavl <112695803+devNikolasPavl@users.noreply.github.com>
2024-09-26 14:09:59 +0300

    video_02, ch_02, lesson_02

---
+++ ru/video_02/level_01/ch_02/lesson_02/For2.java
@@ -0,0 +1,10 @@
+package ru.video_02.level_01.ch_02.lesson_02;
+
+public class For2 {
+    public static void main(String[] args) {
+
+        for (var i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }
+    }
+}
