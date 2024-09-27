0f60cfcfb629b8b0842a43edf07e9e9f4475a024
devNikolasPavl <112695803+devNikolasPavl@users.noreply.github.com>
2024-09-27 15:32:45 +0300

    video_02, ch_02, lesson_04 дополнение

--- ru/video_02/level_01/ch_02/lesson_04/DisplayNumbersFrom100To1.java
+++ ru/video_02/level_01/ch_02/lesson_04/DisplayNumbersFrom100To1.java
@@ -3,10 +3,12 @@ package ru.video_02.level_01.ch_02.lesson_04;
 public class DisplayNumbersFrom100To1 {
     public static void main(String[] args) {
 
+        // чтение исходных данных
         var from = 100;
         var to = 1;
         var step = 1;
 
+        // решение и вывод результата
         for (int i = from; i >= to; i-=step) {
             System.out.print(i + " ");
         }
