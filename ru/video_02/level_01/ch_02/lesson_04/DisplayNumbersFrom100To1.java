aa6c9ff29519dc9343d80df366f9797ad87bc217
devNikolasPavl <112695803+devNikolasPavl@users.noreply.github.com>
2024-09-27 15:35:31 +0300

    video_02, ch_02, lesson_04 дополнение

--- ru/video_02/level_01/ch_02/lesson_04/DisplayNumbersFrom100To1.java
+++ ru/video_02/level_01/ch_02/lesson_04/DisplayNumbersFrom100To1.java
@@ -4,6 +4,8 @@ public class DisplayNumbersFrom100To1 {
     public static void main(String[] args) {
 
         // чтение исходных данных
+        // благодоря этим данным, если нам понадобится вывести результат
+        // от 100 до 60, то мы поменяем одно значение var to = 60;
         var from = 100;
         var to = 1;
         var step = 1;
