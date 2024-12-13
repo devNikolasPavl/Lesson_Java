4db459748ddbddedf22c6283852bbaba42fb4abc
Nikolas Pavlov <devnikolaspavl@gmail.com>
2024-12-13 09:39:03 +0300

    Первый коммит

---
+++ src/ru/books_01/chapter_01/MyFirstApp.java
@@ -0,0 +1,10 @@
+package ru.books_01.chapter_01;
+
+public class MyFirstApp {
+    public static void main(String[] args) {
+        System.out.print("Я управляю ");
+        System.out.println("миром!");
+        System.out.println("Я управляю");
+        System.out.println("миром");
+    }
+}
