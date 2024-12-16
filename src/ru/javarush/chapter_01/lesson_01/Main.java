716c1c2a1e697eea324da5a8b89d707f564c51be
Nikolas Pavlov <devnikolaspavl@gmail.com>
2024-12-16 15:04:16 +0300

    Добавление новых проектов

---
+++ src/ru/javarush/chapter_01/lesson_01/Main.java
@@ -0,0 +1,8 @@
+package ru.javarush.chapter_01.lesson_01;
+
+public class Main {
+    public static void main(String[] args) {
+        // написать тут ваш код
+        System.out.println("Привет мир!");
+    }
+}
---
+++ src/ru/javarush/chapter_01/lesson_02/Main.java
@@ -0,0 +1,11 @@
+package ru.javarush.chapter_01.lesson_02;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.print("Amigo ");
+        System.out.print("The ");
+        System.out.print("Best");
+    }
+}
